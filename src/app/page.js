69c50255d91bce4c91a792028516ac0a14106747
eason-shinetech <zhaost@shinetechsoftware.com>
2025-03-04 17:26:41 +0800

    page content

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,9 @@
+"use client";
+import Hero from "@/components/Hero";
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Image from "next/image";
+
+const Hero = () => {
+  return (
+    <div className="h-screen grid place-items-center">
+      <div>
+        <div className="flex flex-col items-center justify-center gap-y-3 font-light capitalize">
+          <div className="flex items-center justify-center">
+            <Image
+              src={"/person.png"}
+              alt="person"
+              width={400}
+              height={400}
+              priority={true} //Tell nextjs that this image is important and that it should be loaded as soon as possible
+              className="h-auto w-[150px]"
+            />
+            <span className=" absolute text-3xl font-semibold text-white">
+              Hi
+            </span>
+          </div>
+          <h1 className="text-center text-3xl font-bold text-gray-500 tracking-wider">
+            My name is Eason &
+          </h1>
+          <p className="text-lg tracking-wider text-gray-700">
+            I like animations 😘
+          </p>
+        </div>
+        <div>
+          <a href="#">Icon</a>
+        </div>
+        <a
+          href="#"
+          className="mx-auto mt-7 block w-max rounded-lg px-3 py-1 bg-red-400 font-light capitalize tracking-wider text-white hover:bg-red-500 transition-colors"
+        >
+          Talk to me
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
